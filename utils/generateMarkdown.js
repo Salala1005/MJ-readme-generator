4b939f337879b07671bf5f25f6bd81d1bca73f3b
salala1005 <jin.mijeong@gmail.com>
2024-01-13 12:23:06 +0000

    toc added

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,37 @@
 // function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown(answers) {
+  return `# ${answers.title}
+  ## Table-of-contents
+  * [Description](#description)
+  * [License](#license)
+  * [Usage](#usage)
+  * [Installation](#installation)
+  * [Contributing](#contributing)
+  * [Test](#test)
+  * [Questions](#questions)
+  
+  [Description](#table-of-content)<br />
+  ${answers.description}
+
+  [License](#table-of-content)<br />
+  ${answers.license}
+
+  [Usage](#table-of-content)<br />
+  ${answers.usage}
+
+  [Installation](#table-of-content)<br />
+  ${answers.installation}
+
+  [Contributing](#table-of-content)<br />
+  ${answers.contributing}
+
+  [Test](#table-of-content)<br />
+  ${answers.test}
+
+  [Questions](#table-of-content)<br />
+  Please contact me at <br />
+  [GitHub](https://github.com/${answers.github})<br />
+  [Email](${answers.email})
 
 `;
 }
